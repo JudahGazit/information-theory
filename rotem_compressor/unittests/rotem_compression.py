eb58b0dda69ee5273ba3d678100da20b55fb2670
Rotem Gazit <gazitrotem@gmail.com>
2020-02-07 21:21:58 +0200

    cleanup

--- rotem_compressor/unittests/rotem_compression.py
+++ rotem_compressor/unittests/rotem_compression.py
@@ -8,11 +8,6 @@ from rotem_compressor.unittests.compression_testcase import CompressionTestCase
 class RotemCompressionTests(CompressionTestCase):
     compressor = RotemCompressor()
 
-    @classmethod
-    def setUpClass(cls):
-        with open('../../test/dickens.txt', 'rb') as f:
-            cls.data = f.read(10000)
-
     def test_size_less_than_zip(self):
         rotem_compression = self.compressor.compress(self.data)
         zip_compression = gzip.compress(self.data)
