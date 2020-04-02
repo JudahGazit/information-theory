3acaeb39db51dcce2d1d195dce3df1e90d37381f
Rotem Gazit <gazitrotem@gmail.com>
2020-04-02 10:51:51 +0300

    refactor lzw

--- rotem_compressor/lzw.py
+++ rotem_compressor/lzw.py
@@ -6,37 +6,56 @@ from rotem_compressor.utils import *
 
 
 class LZW(ICompressor):
+    initial_dictionary_size = 256
+
     def __init__(self, maximum_table_size=2 ** 24, raw_values=True):
         self.maximum_table_size = maximum_table_size
         self.raw_values = raw_values
 
-    def compress(self, data):
-        dictionary_size = 256
-        dictionary = {chr(i): i for i in range(dictionary_size)}
-        string = ""
-        compressed_data = []
-        for symbol in data:
-            string_plus_symbol = string + chr(symbol)
-            if string_plus_symbol in dictionary:
-                string = string_plus_symbol
-            else:
-                compressed_value = dictionary[string] if self.raw_values else encode_number(dictionary[string], math.ceil(math.log2(dictionary_size)))
-                compressed_data.append(compressed_value)
-                if len(dictionary) <= self.maximum_table_size:
-                    dictionary[string_plus_symbol] = dictionary_size
-                    dictionary_size += 1
-                string = chr(symbol)
+    def add_code_to_dictionary(self, dictionary, combined_symbols):
+        if len(dictionary) <= self.maximum_table_size:
+            dictionary[combined_symbols] = len(dictionary)
+        
+    def get_code_from_dictionary(self, dictionary, string):
+        if self.raw_values:
+            return dictionary[string]
+        else:
+            code_width = math.ceil(math.log2(len(dictionary)))
+            return encode_number(dictionary[string], code_width)
 
-        if string in dictionary:
-            compressed_value = dictionary[string] if self.raw_values else encode_number(dictionary[string], math.ceil(math.log2(dictionary_size)))
+    def code_symbol(self, compressed_data, dictionary, prev_symbols, symbol):
+        combined_symbols = prev_symbols + chr(symbol)
+        if combined_symbols in dictionary:
+            prev_symbols = combined_symbols
+        else:
+            compressed_value = self.get_code_from_dictionary(dictionary, prev_symbols)
             compressed_data.append(compressed_value)
+            self.add_code_to_dictionary(dictionary, combined_symbols)
+            prev_symbols = chr(symbol)
+        return prev_symbols
+
+    def code_last_symbols_if_necessary(self, compressed_data, dictionary, prev_symbols):
+        if prev_symbols in dictionary:
+            compressed_value = self.get_code_from_dictionary(dictionary, prev_symbols)
+            compressed_data.append(compressed_value)
+
+    def encode_result(self, compressed_data):
         if not self.raw_values:
             compressed_data = bits_to_numbers(''.join(compressed_data))
         return compressed_data
 
+    def compress(self, data):
+        dictionary = {chr(i): i for i in range(self.initial_dictionary_size)}
+        prev_symbols = ""
+        compressed_data = []
+        for symbol in data:
+            prev_symbols = self.code_symbol(compressed_data, dictionary, prev_symbols, symbol)
+        self.code_last_symbols_if_necessary(compressed_data, dictionary, prev_symbols)
+        return self.encode_result(compressed_data)
+
     def decompress(self, compressed):
         decompresed = []
-        dictionary_size = 256
+        dictionary_size = self.initial_dictionary_size
         dictionary_inv = {i: chr(i) for i in range(dictionary_size)}
         dictionary = {chr(i): i for i in range(dictionary_size)}
         string = ""  # String is null.
