d64be307a016988f43fe2e7a9d3378c0112a5bb9
Rotem Gazit <gazitrotem@gmail.com>
2020-04-03 10:12:06 +0300

    adding setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rotem-compressor',
+    version='1.0',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='Rotem Gazit',
+    author_email='gazitrotem@gmail.com',
+    description='',
+)
